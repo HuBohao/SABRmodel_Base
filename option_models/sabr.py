6450b506af2256a93eac23b5e897166bfc5bf185
Jaehyuk Choi <github@jae.eml.cc>
2017-10-11 18:03:38 +0800

    SABR for 0<Beta<=1 implemented

--- option_models/sabr.py
+++ option_models/sabr.py
@@ -18,22 +18,23 @@ def bsm_vol(strike, forward, texp, sigma, alpha=0, rho=0, beta=1):
     powFwdStrk = (forward*strike)**((1-beta)/2)
     logFwdStrk = np.log(forward/strike)
     logFwdStrk2 = logFwdStrk**2
-  
+    rho2 = rho*rho
+
     pre1 = powFwdStrk*( 1 + (1-beta)**2/24 * logFwdStrk2*(1 + (1-beta)**2/80 * logFwdStrk2) )
   
     pre2alp0 = (2-3*rho2)*alpha**2/24
     pre2alp1 = alpha*rho*beta/4/powFwdStrk
     pre2alp2 = (1-beta)**2/24/powFwdStrk**2
-  
+
     pre2 = 1 + texp*( pre2alp0 + sigma*(pre2alp1 + pre2alp2*sigma) )
 
     zz = powFwdStrk*logFwdStrk*alpha/np.fmax(sigma, 1e-32)  # need to make sure sig > 0
     if isinstance(zz, float):
         zz = np.array([zz])
-    yy = sqrt(1 + zz*(zz-2*rho))
-  
+    yy = np.sqrt(1 + zz*(zz-2*rho))
+
     xx_zz = np.zeros(zz.size)
-  
+
     ind = np.where(abs(zz) < 1e-5)
     xx_zz[ind] = 1 + (rho/2)*zz[ind] + (1/2*rho2-1/6)*zz[ind]**2 + 1/8*(5*rho2-3)*rho*zz[ind]**3
     ind = np.where(zz >= 1e-5)
@@ -73,6 +74,42 @@ def norm_vol(strike, forward, texp, sigma, alpha=0, rho=0):
  
     return(nvol[0] if nvol.size==1 else nvol)
 
+class ModelHagan:
+    alpha, beta, rho = 0.0, 1, 0.0
+    texp, sigma, intr, divr = None, None, None, None
+    bsm_model = None
+    
+    def __init__(self, texp, sigma, alpha=0, rho=0.0, beta=1.0, intr=0, divr=0):
+        self.beta = beta
+        self.texp = texp
+        self.sigma = sigma
+        self.alpha = alpha
+        self.rho = rho
+        self.intr = intr
+        self.divr = divr
+        self.bsm_model = bsm.Model(texp, sigma, intr=intr, divr=divr)
+        
+    def bsm_vol(self, strike, spot, texp=None, sigma=None):
+        sigma = self.sigma if(sigma is None) else sigma
+        texp = self.texp if(texp is None) else texp
+        forward = spot * np.exp(texp*(self.intr - self.divr))
+        return bsm_vol(strike, forward, texp, sigma, alpha=self.alpha, beta=self.beta, rho=self.rho)
+        
+    def price(self, strike, spot, texp=None, sigma=None, cp_sign=1):
+        bsm_vol = self.bsm_vol(strike, spot, texp, sigma)
+        return self.bsm_model.price(strike, spot, texp, bsm_vol, cp_sign=cp_sign)
+    
+    def impvol(self, price, strike, spot, texp=None, cp_sign=1, setval=False):
+        texp = self.texp if(texp is None) else texp
+        vol = self.bsm_model.impvol(price, strike, spot, texp, cp_sign=cp_sign)
+        forward = spot * np.exp(texp*(self.intr - self.divr))
+        
+        iv_func = lambda _sigma: \
+            bsm_vol(strike, forward, texp, _sigma, alpha=self.alpha, rho=self.rho) - vol
+        sigma = sopt.brentq(iv_func, 0, 10)
+        if(setval):
+            self.sigma = sigma
+        return sigma
 
 class ModelNormalHagan:
     alpha, beta, rho = 0.0, 0.0, 0.0
@@ -99,7 +136,7 @@ class ModelNormalHagan:
         n_vol = self.norm_vol(strike, spot, texp, sigma)
         return self.normal_model.price(strike, spot, texp, n_vol, cp_sign=cp_sign)
     
-    def impvol(self, price, strike, spot, texp=None, cp_sign=1):
+    def impvol(self, price, strike, spot, texp=None, cp_sign=1, setval=False):
         texp = self.texp if(texp is None) else texp
         vol = self.normal_model.impvol(price, strike, spot, texp, cp_sign=cp_sign)
         forward = spot * np.exp(texp*(self.intr - self.divr))
@@ -107,4 +144,6 @@ class ModelNormalHagan:
         iv_func = lambda _sigma: \
             norm_vol(strike, forward, texp, _sigma, alpha=self.alpha, rho=self.rho) - vol
         sigma = sopt.brentq(iv_func, 0, 50)
+        if(setval):
+            self.sigma = sigma
         return sigma
